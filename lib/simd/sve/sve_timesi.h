f6e802dd69fae0959a96973fc5e91277e23f8653
nmeyer-ur <nils.meyer@ur.de>
2019-12-19 12:27:21 +0100

    typos

--- lib/simd/sve/sve_timesi.h
+++ lib/simd/sve/sve_timesi.h
@@ -28,7 +28,7 @@
     inline vec<T> operator()(const vec<T> &a, const vec<T> &b){
 
       vec<T> out;
-      vec<T> tbl1 = acle<T>::tlb1();
+      const vec<T> tbl1 = acle<T>::tbl1();
       svbool_t pg1 = acle<T>::pg1();
       svbool_t pg_even = acle<T>::pg_even();
 
--- lib/simd/sve/sve_timesminusi.h
+++ lib/simd/sve/sve_timesminusi.h
@@ -9,11 +9,14 @@
     inline vec<T> operator()(const vec<T> &a, const vec<T> &b){
 
       vec<T> out;
+      const vec<T> tbl1 = acle<T>::tbl1();
       svbool_t pg1 = acle<T>::pg1();
+      svbool_t pg_odd = acle<T>::pg_odd();
+
+      typename acle<T>::svuint tbl1_v = svld1(pg1, tbl1.v);
       typename acle<T>::vt a_v = svld1(pg1, a.v);
-      typename acle<T>::vt re_v = svtrn1(a_v, a_v);
-      re_v = svneg_x(pg1, re_v);
-      typename acle<T>::vt r_v = svtrn2(a_v, re_v);
+      a_v = svtbl(a_v, tbl1_v);
+      typename acle<T>::vt r_v = svneg_x(pg_odd, a_v);
       svst1(pg1, out.v, r_v);
 
       return out;
