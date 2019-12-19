7fac1829cc3f3556b95bcfc105115ba82ece111d
nmeyer-ur <nils.meyer@ur.de>
2019-12-19 12:19:31 +0100

    instruction reordering

--- lib/simd/sve/sve_timesi.h
+++ lib/simd/sve/sve_timesi.h
@@ -29,10 +29,10 @@
 
       vec<T> out;
       vec<T>& tbl1 = acle<T>::tlb1();
-      typename acle<T>::svuint tbl1_v = svld1(pg1, tbl1.v);
-
       svbool_t pg1 = acle<T>::pg1();
       svbool_t pg_even = acle<T>::pg_even();
+
+      typename acle<T>::svuint tbl1_v = svld1(pg1, tbl1.v);
       typename acle<T>::vt a_v = svld1(pg1, a.v);
       a_v = svtbl(a_v, tbl1_v);
       typename acle<T>::vt r_v = svneg_x(pg_even, a_v);
