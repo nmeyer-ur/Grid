ec16aeb2488825f4dfb6ab6af4429d346b0ae5b5
nmeyer-ur <nils.meyer@ur.de>
2019-12-19 12:23:11 +0100

    typos

--- lib/simd/sve/sve_acle.h
+++ lib/simd/sve/sve_acle.h
@@ -29,9 +29,9 @@
     //static inline svbool_t pg4(){return svptrue_pat_b16(SV_VL8);}
     static inline svbool_t pg4(){return svptrue_pat_b64(SV_VL2);}
     // exchange neighboring elements
-    static inline vec<double>& tbl1(){
-        vec<double> t = {1, 0, 3, 2, 5, 4, 7, 6};
-        return &t;
+    static inline vec<double> tbl1(){
+        const vec<double> t = {1, 0, 3, 2, 5, 4, 7, 6};
+        return t;
     }
     #elif (GEN_SIMD_WIDTH == 32u)
     //static inline svbool_t pg2(){return svptrue_pat_b16(SV_VL8);}
@@ -64,9 +64,9 @@
     //static inline svbool_t pg2(){return svptrue_pat_b16(SV_VL16);}
     static inline svbool_t pg2(){return svptrue_pat_b32(SV_VL8);}
     // exchange neighboring elements
-    static inline vec<float>& tbl1(){
-        vec<double> t = {1, 0, 3, 2, 5, 4, 7, 6, 9, 8, 11, 10, 13, 12, 15, 14};
-        return &t;
+    static inline vec<float> tbl1(){
+        const vec<float> t = {1, 0, 3, 2, 5, 4, 7, 6, 9, 8, 11, 10, 13, 12, 15, 14};
+        return t;
     }
     #elif (GEN_SIMD_WIDTH == 32u)
     //static inline svbool_t pg2(){return svptrue_pat_b16(SV_VL8);}
