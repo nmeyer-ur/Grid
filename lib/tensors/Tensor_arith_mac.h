f73af5589a3665eefb61de4dc5e8a833b89eebdc
nmeyer-ur <nils.meyer@ur.de>
2020-02-14 10:54:40 +0100

    mac0/1 in outer loop

--- lib/tensors/Tensor_arith_mac.h
+++ lib/tensors/Tensor_arith_mac.h
@@ -84,6 +84,8 @@ strong_inline void mac(iMatrix<rrtype,N> * __restrict__ ret,const iMatrix<ltype,
                 mac0(&ret->_internal[c1][c2],&lhs->_internal[c1][c3],&rhs->_internal[c3][c2]);
             }
         }
+    }
+    for(int c3=0;c3<N;c3++){
         for(int c1=0;c1<N;c1++){
             for(int c2=0;c2<N;c2++){
                 mac1(&ret->_internal[c1][c2],&lhs->_internal[c1][c3],&rhs->_internal[c3][c2]);
